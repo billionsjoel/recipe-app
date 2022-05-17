07746181eec7893cd4810bc8679af8c4fc3525e4
joskalenda <joskalenda3@gmail.com>
2022-05-17 15:17:42 +0200

    Upate routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'recipes/index'
   get 'recipes/show'
+  get 'recipes/new'
   devise_for :users
+
+  resources :recipes, only: [:index, :show, :create, :new]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
