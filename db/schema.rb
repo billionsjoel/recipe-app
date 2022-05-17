bdedcea0dcf53a7cb401c4ba8906af0cae100874
Billions Joel <joelatugonza@gmail.com>
2022-05-17 15:10:58 +0300

    Generate model food and table

--- db/schema.rb
+++ db/schema.rb
@@ -14,15 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_115807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.integer "price"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
