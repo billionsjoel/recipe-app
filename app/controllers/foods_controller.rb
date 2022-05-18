c53c83889187a9962855e718f5eb3e4bb1ccca05
Billions Joel <joelatugonza@gmail.com>
2022-05-18 17:56:45 +0300

    Fix linter

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -26,7 +26,7 @@ class FoodsController < ApplicationController
 
   def delete
     Food.find(params[:id]).destroy
-    redirect_to :action => 'index'
+    redirect_to foods_path
   end
 
   def food_params
